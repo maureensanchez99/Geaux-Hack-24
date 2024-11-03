645ca96993a74a5db81ac4df14402cc9de81aeb8
Jp <gonzalez.jean1503@gmail.com>
2024-11-03 08:09:49 -0600

    Finsihed Login

--- PocketTravel/Components/User.cs
+++ PocketTravel/Components/User.cs
@@ -2,19 +2,21 @@ public class c_currentUser
 {
     public string Id {get; set;}
 
-    public string Email { get; set; }
+    public string Email {get; set;}
+    public string UserName {get; set;}
 
     public string fullName {get; set;}
 
     public string phoneNumber {get; set;}
 
-    public bool IsLoggedIn { get; set; } = false;
+    public bool IsLoggedIn { get; set;} = false;
 
-    public void SetUser(string id,string email, string fullname, string phonenumber)
+    public void SetUser(string id,string email, string fullname, string phonenumber, string username)
     {
         Id = id;
         Email = email;
         fullName = fullname;
+        UserName = username;
         phoneNumber = phonenumber;
         IsLoggedIn = true;
     }
@@ -25,6 +27,7 @@ public class c_currentUser
         Email = string.Empty;
         phoneNumber = string.Empty;
         fullName = string.Empty;
+        UserName = string.Empty;
         IsLoggedIn = false;
     }
 }
